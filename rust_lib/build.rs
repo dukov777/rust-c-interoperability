fbce75126b0d062f5f9fa86caa6960197569c804
Petar Lalov <petar.lalov@gmail.com>
2024-12-25 08:44:52 +0200

    Static library for C functions. Added as a separate CMake project for a cleaner build process.

--- rust_lib/build.rs
+++ rust_lib/build.rs
@@ -2,8 +2,8 @@
 
 fn main() {
     // Tell Cargo to link to the C static library
-    println!("cargo:rustc-link-lib=static=other_lib"); // Name of the C static library (without lib prefix or extension)
+    println!("cargo:rustc-link-lib=static=static_c_lib"); // Name of the C static library (without lib prefix or extension)
     
     // Tell Cargo where to find the C library
-    println!("cargo:rustc-link-search=native={}", "../build/"); // Path to the directory containing libother_lib.a
+    println!("cargo:rustc-link-search=native={}", "../c_lib/build"); // Path to the directory containing libother_lib.a
 }
